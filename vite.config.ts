96077e44df9e05580fc5fed05a7c60a793e34b26
zabbonat <43636578+zabbonat@users.noreply.github.com>
2026-01-26 02:32:07 +0100

    Prepare for repository rename to CheckIfExist

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/References-Validation/',
+  base: '/CheckIfExist/',
   build: {
     sourcemap: false,
     chunkSizeWarningLimit: 1000,
